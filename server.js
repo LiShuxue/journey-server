b71fb99ba69b4f458db6215f0be2059b479f4882
Li Shuxue <Shuxue.Li@sc.com>
2018-10-17 21:22:40 +0800

    给静态文件添加缓存

--- server.js
+++ server.js
@@ -35,7 +35,9 @@ app.use(router.routes()).use(router.allowedMethods());
 
 // 该中间件用来提供静态文件服务
 const static = require('koa-static');
-app.use(static(__dirname + '/static'));
+app.use(static(__dirname + '/static', {
+  maxage: 1000 * 60 * 60 * 24 * 365
+}));
 
 app.listen(4000, ()=>{
   console.log('server starting...')
