1abe8ed7a3d7a160fadfd6ed0d1866e7b3e92c24
Li Shuxue <Shuxue.Li@sc.com>
2019-06-14 22:30:35 +0800

    后端数据库不再存储token

--- src/models/User.js
+++ src/models/User.js
@@ -3,15 +3,12 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     username: { unique: true, type: String },
-    password: { type: String },
-    access_token: { type: String },
-    refresh_token: { type: String }
+    password: { type: String }
 },{
     collection: 'User' // 加上这个就不会在程序中定义的是User而真实数据库中变成了Users
 });
 
 const User = mongoose.model('User', userSchema);
-
 
 const createUser = (user) => {
     return User.create(user);
@@ -28,39 +25,7 @@ const getUser = (username) => {
     })
 }
 
-const saveAccessToken = (user, access_token) => {
-    return new Promise((resolve, reject)=>{
-        User.updateOne( { _id: user.id }, {
-            $set: {
-                access_token: access_token
-            }
-        }, (err, doc)=>{
-            if(err){
-                reject(err);
-            }
-            resolve(doc);
-        });
-    });
-}
-
-const saveRefreshToken = (user, refresh_token) => {
-    return new Promise((resolve, reject)=>{
-        User.updateOne( { _id: user.id }, {
-            $set: {
-                refresh_token: refresh_token
-            }
-        }, (err, doc)=>{
-            if(err){
-                reject(err);
-            }
-            resolve(doc);
-        });
-    });
-}
-
 module.exports = {
     createUser,
-    getUser,
-    saveAccessToken,
-    saveRefreshToken
+    getUser
 };
