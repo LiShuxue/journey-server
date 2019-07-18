ef59147f3a862f6fb21c623e2a618150f0234333
LiShuxue <1149926505@qq.com>
2019-07-18 15:10:52 +0800

    更新和删除博客

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -25,80 +25,6 @@ const publishNewBlog = async ( ctx, next ) => {
     await next();
 }
 
-// const saveImage = async ( ctx, next ) => {
-//     let { path, mimetype, filename, originalname } = ctx.req.file;
-//     let tmp_path = path;
-//     let type = mimetype.substring(6);
-//     let target_path = path + '.' + type;
-//     let src = fs.createReadStream(tmp_path);
-//     let dest = fs.createWriteStream(target_path);
-//     await new Promise((resolve, reject)=>{
-//         src.pipe(dest);
-//         src.on('end', ()=>{
-//             fs.unlinkSync(tmp_path);
-//             resolve();
-//         });
-//         src.on('error', (err)=>{ 
-//             src.close();
-//             dest.close();
-//             reject(err);
-//         });
-//     }).catch(err=>{
-//         ctx.status = 200;
-//         ctx.body = {
-//             errMsg: '图片上传失败!',
-//             err
-//         }
-//     });
-//     let serverPath = process.env.NODE_ENV === 'production' ? 'http://lishuxue.site/' : 'http://localhost:4000/';
-//     let imagePath = serverPath + filename + '.' + type;
-//     ctx.status = 200;
-//     ctx.body = {
-//         successMsg: '图片上传成功!',
-//         originalName: originalname,
-//         serverFileName: filename + '.' + type,
-//         imagePath
-//     }
-//     await next();
-// }
-
-// const removeImage = async (ctx, next) => {
-//     let isExist;
-//     let path;
-//     if (ctx.request.body.filename){
-//         path = 'static/' + ctx.request.body.filename;
-//         isExist = fs.existsSync(path);
-//     }
-
-//     if(path && isExist){
-//         await new Promise((resolve, reject)=>{
-//             fs.unlink(path, (err)=>{
-//                 if(err){
-//                     reject(err)
-//                 }
-//                 resolve()
-//             });
-//         }).catch(err=>{
-//             ctx.status = 500;
-//             ctx.body = {
-//                 errMsg: '图片删除失败!',
-//                 err
-//             }
-//         });
-//         ctx.status = 200;
-//         ctx.body = {
-//             successMsg: '图片删除成功!',
-//         }
-//     }else{
-//         ctx.status = 200;
-//         ctx.body = {
-//             errMsg: '图片不存在!'
-//         }
-//     }
-    
-//     await next();
-// }
-
 const getAllBlog = async (ctx) => {
     try {
         let blogList = await BlogModel.getAllBlog();
@@ -116,44 +42,45 @@ const getAllBlog = async (ctx) => {
     }
 }
 
-// const getHotBlog = async (ctx) => {
-//     let blogList = await BlogModel.getAllBlogSortBySee().catch(err=>{
-//         ctx.status = 500;
-//         ctx.body = {
-//             errMsg: '获取博客列表失败!',
-//             err
-//         }
-//     });
-//     ctx.status = 200;
-//     ctx.body = {
-//         successMsg: '获取博客列表成功!',
-//         blogList
-//     }
-// }
-
-// const getAllTags = async (ctx) => {
-//     let result = await BlogModel.getAllTags().catch(err=>{
-//         ctx.status = 500;
-//         ctx.body = {
-//             errMsg: '获取标签失败!',
-//             err
-//         }
-//     })
-//     let tagList=[];
-//     result.forEach((value)=>{
-//         // 用Set数组去重
-//         tagList = [...tagList, ...value.tags];
-//     })
-//     ctx.status = 200;
-//     ctx.body = {
-//         successMsg: '获取标签成功!',
-//         tagList: [...new Set(tagList)]
-//     }
-// }
+const deleteBlog = async (ctx, next) => {
+    try {
+        let ids = ctx.request.body.ids;
+        await BlogModel.deleteAllBlog(ids);
+        
+        ctx.status = 200;
+        ctx.body = {
+            successMsg: '删除博客成功!'
+        }
+    } catch (err) {
+        ctx.status = 500;
+        ctx.body = {
+            errMsg: '删除博客失败!',
+            err
+        }
+    }
+    await next();
+}
+const updateBlog = async (ctx, next) => {
+    try {
+        let blog = ctx.request.body.blog;
+        await BlogModel.updateBlog(blog._id, blog);
+        ctx.status = 200;
+        ctx.body = {
+            successMsg: '更新博客成功!'
+        }
+    } catch (err) {
+        ctx.status = 500;
+        ctx.body = {
+            errMsg: '更新博客失败!',
+            err
+        }
+    }
+    await next();
+}
 
 module.exports = {
     publishNewBlog,
-    // saveImage,
-    // removeImage,
-    getAllBlog
+    getAllBlog,
+    deleteBlog,
+    updateBlog
 };
