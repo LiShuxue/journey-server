1094f32beff8b7f592def05cf2ec606a8d2e7854
LiShuxue <1149926505@qq.com>
2019-07-22 16:19:07 +0800

    添加sentry

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,6 +1,8 @@
 const BlogModel = require('../models/Blog');
+const sentry = require('../utils/sentry');
 
 const publishNewBlog = async ( ctx, next ) => {
+    sentry.addBreadcrumb('controllers/blogController.js --> publishNewBlog');
     let blog = ctx.request.body.blog;
     blog.publishTime = Date.now();
     blog.updateTime = Date.now();
@@ -15,6 +17,7 @@ const publishNewBlog = async ( ctx, next ) => {
             successMsg: '文章发表成功!'
         }
     } catch (err) {
+        sentry.captureException(err);
         ctx.status = 500;
         ctx.body = {
             errMsg: '发表文章失败!',
@@ -26,6 +29,7 @@ const publishNewBlog = async ( ctx, next ) => {
 }
 
 const getAllBlog = async (ctx) => {
+    sentry.addBreadcrumb('controllers/blogController.js --> getAllBlog');
     try {
         let blogList = await BlogModel.getAllBlog();
         ctx.status = 200;
@@ -34,6 +38,7 @@ const getAllBlog = async (ctx) => {
             blogList
         }
     } catch (err) {
+        sentry.captureException(err);
         ctx.status = 500;
         ctx.body = {
             errMsg: '获取博客列表失败!',
@@ -43,6 +48,7 @@ const getAllBlog = async (ctx) => {
 }
 
 const deleteBlog = async (ctx, next) => {
+    sentry.addBreadcrumb('controllers/blogController.js --> deleteBlog');
     try {
         let ids = ctx.request.body.ids;
         await BlogModel.deleteAllBlog(ids);
@@ -52,6 +58,7 @@ const deleteBlog = async (ctx, next) => {
             successMsg: '删除博客成功!'
         }
     } catch (err) {
+        sentry.captureException(err);
         ctx.status = 500;
         ctx.body = {
             errMsg: '删除博客失败!',
@@ -61,6 +68,7 @@ const deleteBlog = async (ctx, next) => {
     await next();
 }
 const updateBlog = async (ctx, next) => {
+    sentry.addBreadcrumb('controllers/blogController.js --> updateBlog');
     try {
         let blog = ctx.request.body.blog;
         await BlogModel.updateBlog(blog._id, blog);
@@ -69,6 +77,7 @@ const updateBlog = async (ctx, next) => {
             successMsg: '更新博客成功!'
         }
     } catch (err) {
+        sentry.captureException(err);
         ctx.status = 500;
         ctx.body = {
             errMsg: '更新博客失败!',
