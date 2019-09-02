7c7ebf8ef789efa21f24799b99226df491b96da1
LiShuxue <1149926505@qq.com>
2019-09-02 12:19:46 +0800

    移除category 相关接口

--- src/routes/categoryRoute.ts
+++
@@ -1,9 +0,0 @@
-import Router from 'koa-router';
-import CategoryController from '../controllers/categoryController';
-
-const categoryRoute = new Router();
-
-categoryRoute.post('/add', CategoryController.addCategory);
-categoryRoute.get('/list', CategoryController.getAllCategory);
-
-export default categoryRoute;
