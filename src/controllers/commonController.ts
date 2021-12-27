66850a7da177c62d201444bd005668fceb9ac3ac
LiShuxue <1149926505@qq.com>
2021-12-27 20:02:28 +0800

    更新天气服务接口，使用腾讯的

--- src/controllers/commonController.ts
+++ src/controllers/commonController.ts
@@ -8,17 +8,20 @@ const getHomeInfo = async (ctx: Context): Promise<any> => {
   try {
     const ip = ctx.request.ip;
     const one = await loadWebPage();
-    // 腾讯位置服务，根据IP获取城市信息
+    // 腾讯位置服务，根据IP获取城市信息 https://lbs.qq.com/service/webService/webServiceGuide/webServiceIp
     const positionUrl = `https://apis.map.qq.com/ws/location/v1/ip?ip=${ip}&key=GWSBZ-HTTWJ-QJIF2-KLHLC-X3WKV-ZZFV6`;
-    const cityinfo = await axios.get(encodeURI(positionUrl));
-    const city = cityinfo.data.result ? cityinfo.data.result.ad_info.city.replace('市', '') : '北京';
-    // 根据城市获取天气预报信息
-    const url = `https://www.tianqiapi.com/free/day?appid=19838913&appsecret=dUknzCP2&city=${city}`;
+    const addressInfo = await axios.get(encodeURI(positionUrl));
+    const province = addressInfo.data.result ? addressInfo.data.result.ad_info.province : '';
+    const city = addressInfo.data.result ? addressInfo.data.result.ad_info.city : '';
+    const district = addressInfo.data.result ? addressInfo.data.result.ad_info.district : '';
+    // 腾讯天气服务，根据城市获取天气预报信息
+    const url = `https://wis.qq.com/weather/common?source=pc&weather_type=observe|tips&province=${province}&city=${city}&county=${district}`;
     const wea = await axios.get(encodeURI(url));
     ctx.status = 200;
     ctx.body = {
       one,
-      wea: wea.data
+      address: addressInfo.data.result.ad_info,
+      wea: wea.data.data
     };
   } catch (err) {
     sentry.captureException(err);
