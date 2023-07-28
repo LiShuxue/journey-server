ec383c53956dafad1d4fd5e6c79af5d46dee53b9
LiShuxue <1149926505@qq.com>
2023-07-28 11:40:01 +0800

    升级sentry

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,63 +1,23 @@
-const Sentry = require('@sentry/node');
+import * as Sentry from '@sentry/node';
 import logger from './logger';
 
-interface Scope {
-  // eslint-disable-next-line
-  setUser: <T>(arg: T) => void;
-  // eslint-disable-next-line
-  setTag: <T, U>(arg1: T, arg2: U) => void;
-  // eslint-disable-next-line
-  setExtra: <T, U>(arg1: T, arg2: U) => void;
-}
+Sentry.init({
+  dsn: 'https://c0cc657e1c54459eb8b51d60df18a121@o212666.ingest.sentry.io/1510147',
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0
+});
 
-type LogLevel = 'fatal' | 'error' | 'warning' | 'info' | 'debug';
-
-if (process.env.NODE_ENV === 'production') {
-  Sentry.init({ dsn: 'https://c0cc657e1c54459eb8b51d60df18a121@sentry.io/1510147' });
-}
-
-const setUserContext = (context: any) => {
-  Sentry.configureScope((scope: Scope) => {
-    scope.setUser(context);
-  });
-};
-
-const setTagContext = (key: string, value: string) => {
-  Sentry.configureScope((scope: Scope) => {
-    scope.setTag(key, value);
-  });
-};
-
-const setExtraContext = (key: string, obj: any) => {
-  Sentry.configureScope((scope: Scope) => {
-    scope.setExtra(key, obj);
-  });
-};
-
-const addBreadcrumb = (message: string, data?: any, category: string = 'breadcrumb', level: LogLevel = 'info') => {
+const addBreadcrumb = (message: string) => {
   logger.info(message);
-  Sentry.addBreadcrumb({
-    message,
-    data,
-    category: category,
-    level: level
-  });
+  Sentry.addBreadcrumb({ message });
 };
 
 const captureException = (error: any) => {
   logger.error(error);
-  error && Sentry.captureException(error);
+  Sentry.captureException(error);
 };
-
-const captureMessage = (msg: string) => {
-  msg && Sentry.captureMessage(msg);
-};
-
 export default {
-  setUserContext,
-  setTagContext,
-  setExtraContext,
   addBreadcrumb,
-  captureException,
-  captureMessage
+  captureException
 };
