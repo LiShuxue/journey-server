359d2ce6109fb0d8bacb6819c5838397520518b3
LiShuxue <1149926505@qq.com>
2023-08-15 15:29:39 +0800

    增加dto

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, ForbiddenException, Get, Param, UseGuards } from '@nestjs/common';
+import { Body, Controller, ForbiddenException, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { ValidationPipe } from 'src/pipes/validation.pipe';
+import { UserDto } from './user.dto';
 
 /*
   使用 @Controller('user') 注解声明一个controller，处理 user 路径下的请求，类里面的每一个方法，处理一个请求。
@@ -19,17 +20,22 @@ export class UserController {
   // 当控制器被实例化的时候，UserService会被注入，自动实例化userService（默认是单例的，如果已经存在，直接返回）
   constructor(private readonly userService: UserService) {}
 
+  @Post()
+  async create(@Body() userDto: UserDto) {
+    return 'This action adds a new user';
+  }
+
   @Get('list')
-  userList() {
+  async userList() {
     console.log('UserController method');
-    const userList = this.userService.getUserList();
+    const userList = await this.userService.getUserList();
     return userList;
   }
 
   @Get('list/:id')
-  getUser(@Param('id', ValidationPipe) id: number) {
+  async getUser(@Param('id', ValidationPipe) id: number) {
     console.log('UserController method');
-    const user = this.userService.getUser(id);
+    const user = await this.userService.getUser(id);
     return user;
   }
 
---
+++ src/modules/user/user.dto.ts
@@ -0,0 +1,5 @@
+// 实体，数据传输时接收
+export class UserDto {
+  readonly username: string;
+  readonly password: string;
+}
