1f773228e59dd0562d3de3d237fbafefae1174cd
LiShuxue <1149926505@qq.com>
2025-03-15 11:16:48 +0800

    暂停发送邮件

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from '../common/common.module';
 import { EmailModule } from '../email/email.module';
-import { SendEamilTaskService } from './sendEmailTask.service';
+// import { SendEamilTaskService } from './sendEmailTask.service';
 import { BackupTaskService } from './backupTask.service';
 
 @Module({
   imports: [CommonModule, EmailModule],
-  providers: [SendEamilTaskService, BackupTaskService],
+  providers: [BackupTaskService],
 })
 export class TasksModule {}
